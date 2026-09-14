1964482c86880d91f76e357ad8ba8a4b6fa648b4
GabrielSaboia10 <gabriel.saboia14@gmail.com>
2026-09-13 22:34:38 -0300

    Collect endereco and telefone when including a Pessoa

--- SistemaBancarioJAVA/src/controller/CtrlIncluirPessoa.java
+++ SistemaBancarioJAVA/src/controller/CtrlIncluirPessoa.java
@@ -2,36 +2,52 @@ package controller;
 
 import model.ModelException;
 import model.Pessoa;
+import model.Role;
+import model.SenhaUtil;
+import model.Usuario;
 import model.dao.DaoPessoa;
+import model.dao.DaoUsuario;
 
 public class CtrlIncluirPessoa extends CtrlAbstratoPessoa {
 	//
 	// MÉTODOS
 	//
 	public CtrlIncluirPessoa(ICtrl c) {
-		// Executando o construtor de CtrlAbstratoPessoa
 		super(c);
-		// Informo que não há pessoa sendo editada previamente
 		this.setPessoaEmEdicao(null);
 		this.getMeuViewer().apresentar();
 	}
 
-	public void efetuar(String cpf, String nome, int idade) {
+	public void efetuar(String cpf, String nome, int idade, String endereco, String telefone) {
+		Pessoa p;
 		try {
-			Pessoa p = new Pessoa(cpf, nome, idade);
+			p = new Pessoa(cpf, nome, idade, endereco, telefone);
 			this.setPessoaEmEdicao(p);
 		} catch (ModelException e1) {
 			this.getMeuViewer().notificar("Erro: " + e1);
 			return;
 		}
+
 		DaoPessoa dao = new DaoPessoa();
-		boolean inserido = dao.incluir(this.getPessoaEmEdicao());
-		if (inserido) {
-			this.getMeuViewer().notificar("Inclusão da Pessoa " + getPessoaEmEdicao() + " realizada com sucesso!");
-			this.finalizar();
-		} else {
-			// Aqui você mostra a mensagem de erro pra quem estiver usando a aplicação
+		boolean inserido = dao.incluir(p);
+		if (!inserido) {
 			this.getMeuViewer().notificar("Erro ao incluir Pessoa (CPF duplicado ou outro problema).");
+			return;
 		}
+
+		// Auto-cria usuário CLIENTE com senha inicial = últimos 6 chars do CPF
+		String senhaInicial = cpf.substring(cpf.length() - 6);
+		try {
+			Usuario u = new Usuario(cpf, SenhaUtil.hashSha256(senhaInicial), Role.CLIENTE, p);
+			new DaoUsuario().incluir(u);
+			this.getMeuViewer().notificar(
+				"Pessoa " + p + " incluída!\n" +
+				"Usuário criado automaticamente.\nSenha inicial: " + senhaInicial
+			);
+		} catch (ModelException e) {
+			this.getMeuViewer().notificar("Pessoa incluída, mas erro ao criar usuário: " + e.getMessage());
+		}
+
+		this.finalizar();
 	}
 }
